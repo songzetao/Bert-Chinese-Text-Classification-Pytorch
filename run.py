c42d4819fc91ef73c3567813c0d96aa93b5e3d92
songzetao <925507966@qq.com>
2022-06-06 15:39:05 +0800

    convert pytorch to oneflow

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # coding: UTF-8
 import time
-import torch
+# import torch
+import oneflow as torch
 import numpy as np
 from train_eval import train, init_network
 from importlib import import_module
